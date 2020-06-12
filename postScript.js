3e9d5657d05c303857a5da77a1e059f19d9172a5
aimona9 <58906047+aimona9@users.noreply.github.com>
2020-06-12 15:38:20 +0200

    re-eddited the postScript.js file again to call the function from the button and parse in the name of the container form

--- postScript.js
+++ postScript.js
@@ -1,30 +1,33 @@
-const app = document.getElementById('post_test')
+function postData(domQuery)
+{
+    var request = new XMLHttpRequest()
+    request.open('POST', 'https://costume-rental.herokuapp.com/new_reservation', true)
 
-//var data = new FormData(this.response)
+    //Send the proper header information along with the request
+    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
-
-var out = {};
-var s_data = $(app).serializeArray();
-
-for(var i = 0; i<s_data.length; i++)
-    {
-        var record = s_data[i];
-        out[record.name] = record.value;
+    request.onreadystatechange = function() 
+    { // Call a function when the state changes.
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) 
+        {
+            // Request finished. Do processing here.
+            var out = {};
+            s_data = $(dom_query).serializeArray();
+            //transform into simple data/value object
+            for(var i = 0; i<s_data.length; i++)
+            {
+                var record = s_data[i];
+                out[record.name] = record.value;
+            }
+    
+    var data = JSON.stringify(out); 
+        }
+        else
+        {
+            alert("Server not responding. Please try again a little later!")
+        }
     }
-    return out;
 
-
-var data = JSON.stringify(out);
-var request = new XMLHttpRequest()
-request.open('POST', 'https://costume-rental.herokuapp.com/new_reservation', true)
-
-//Send the proper header information along with the request
-request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-request.onreadystatechange = function() { // Call a function when the state changes.
-    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        // Request finished. Do processing here.
-    }
-}
 console.log(data);
 request.send(data);
+}
