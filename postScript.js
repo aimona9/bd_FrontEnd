3e18c595cb4f7d7960f7580ba195bd090db5deaa
aimona9 <58906047+aimona9@users.noreply.github.com>
2020-06-12 19:32:11 +0200

    Used fetch api now to do the connection

--- postScript.js
+++ postScript.js
@@ -1,25 +1,37 @@
-var data = new FormData();
-data.append('pick_up_date', '2020-06-18T16:46:57.389Z');
-data.append('return_date', '2020-06-20T16:46:57.389Z');
-data.append('pick_up_location_id', 0);
-data.append('costumes', [0,1]);
+async function someRequest()
+{
 
-   
-var request = new XMLHttpRequest()
-request.open('POST', "https://costume-rental.herokuapp.com/test_new_resrvation", true)
+    var data = new FormData();
+    data.append('pick_up_date', '2020-06-18T16:46:57.389Z');
+    data.append('return_date', '2020-06-20T16:46:57.389Z');
+    data.append('pick_up_location_id', 0);
+    data.append('costumes', [0,1]); 
+  
+    try
+    {
+        const response = await fetch('https://costume-rental.herokuapp.com/test_new_resrvation', 
+        {
+            method: 'POST',
+            mode:'cors',
+            headers: 
+            {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        const data = await response.json()
+        console.log(data)
+    }
+}  
 
 //Send the proper header information along with the request
-request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+//request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 //request.onreadystatechange = function()
-request.onload = function()  
-{ // Call a function when the state changes.
-    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) 
-    {
-        console.log(this.responseText);
-        data.append('pick_up_date', '2020-06-18T16:46:57.389Z');
-        data.append('return_date', '2020-06-20T16:46:57.389Z');
-        data.append('pick_up_location_id', 0);
-        data.append('costumes', [0,1]); 
+//request.onload = function()  
+//{ // Call a function when the state changes.
+//    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) 
+  //  {
+       
             
             // Request finished. Do processing here.
             //var out = {};
@@ -31,12 +43,12 @@ request.onload = function()
             //    out[record.name] = record.value;
             //}    
             //data = JSON.stringify(out); 
-    }
-    else
-    {
-        alert("Server not responding. Please try again a little later!")
-    }
-}
+    //}
+   // else
+   // {
+    //    alert("Server not responding. Please try again a little later!")
+  //  }
+//}
 
-console.log(data);
-request.send(data);
+//console.log(data);
+//request.send(data);
