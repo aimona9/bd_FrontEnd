944c20724510bb16d06bcf53ec01cda4771cb337
aimona9 <58906047+aimona9@users.noreply.github.com>
2020-06-12 19:36:44 +0200

    Re-editted fetch code

--- postScript.js
+++ postScript.js
@@ -1,28 +1,25 @@
 async function someRequest()
 {
-
     var data = new FormData();
     data.append('pick_up_date', '2020-06-18T16:46:57.389Z');
     data.append('return_date', '2020-06-20T16:46:57.389Z');
     data.append('pick_up_location_id', 0);
     data.append('costumes', [0,1]); 
-  
-    try
+
+    const response = await fetch('https://costume-rental.herokuapp.com/test_new_resrvation', 
     {
-        const response = await fetch('https://costume-rental.herokuapp.com/test_new_resrvation', 
+        method: 'POST',
+        mode:'cors',
+        headers: 
         {
-            method: 'POST',
-            mode:'cors',
-            headers: 
-            {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
+           'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
         const data = await response.json()
         console.log(data)
-    }
-}  
+}
+  
 
 //Send the proper header information along with the request
 //request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
