7de60cc474d41926f0285a6251c9875ff6811185
aimona9 <58906047+aimona9@users.noreply.github.com>
2020-06-15 00:54:02 +0200

    The correct postScript which is now working. Worked when i removed the request.setRequestHeader

--- postScript.js
+++ postScript.js
@@ -1,26 +1,24 @@
 async function someRequest()
 {
     alert("click function was called");
-    var params = new FormData();
-    params.append("pick_up_date", "2007-06-14T18:08:03.314Z");
-    params.append("return_date", "2008-06-24T18:08:03.315Z");
-    params.append("pick_up_location_id",1);
-    params.append("costumes", [1]); 
+    var datePickUp = document.getElementById(date_pickUp);
 
-    var dat = JSON.stringify(params);
+    var dat = JSON.stringify({"pick_up_date": "2007-06-14T18:08:03.314Z","return_date":"2008-06-24T18:08:03.315Z","pick_up_location_id":1
+  , "costumes": [1,3,8]});
     
     //var Base64 = req
     //var Base64 = require('js-base64').Base64;
     var username = "test";
     var password = "admin1";
     //let base64 = require('base-64');
-    var request = new XMLHttpRequest();    
+    var request = new XMLHttpRequest();      
     var url = 'https://costume-rental.herokuapp.com/new_reservation';    
     request.open('POST', url, false, username, password);
     request.withCredentials=true;
     //Send the proper header information along with the request
     //request.setRequestHeader('Authorization', 'Basic ' + Base64.encode(username + ":" + password));
-
+    //request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    //request.setRequestHeader("Content-Type", "application/json");
     request.onreadystatechange = function() 
     {
       //Call a function when the state changes.
@@ -36,34 +34,5 @@ async function someRequest()
       }
     }
     request.send(dat);
-}
-  
-
-//Send the proper header information along with the request
-//request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-//request.onreadystatechange = function()
-//request.onload = function()  
-//{ // Call a function when the state changes.
-//    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) 
-  //  {
-       
-            
-            // Request finished. Do processing here.
-            //var out = {};
-            //s_data = $(dom_query).serializeArray();
-            //transform into simple data/value object
-            //for(var i = 0; i<s_data.length; i++)
-            //{
-            //    var record = s_data[i];
-            //    out[record.name] = record.value;
-            //}    
-            //data = JSON.stringify(out); 
-    //}
-   // else
-   // {
-    //    alert("Server not responding. Please try again a little later!")
-  //  }
-//}
-
-//console.log(data);
-//request.send(data);
+    document.write(dat);
+} 
